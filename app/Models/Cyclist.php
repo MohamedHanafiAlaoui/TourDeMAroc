f62df35c9c38c941f0a150e99d346105ed5fa1fb
ilyass anida <anidailyass@gmail.com>
2025-02-11 15:17:30 +0100

    Implement the signup backend logic.

---
+++ app/Models/Cyclist.php
@@ -0,0 +1,15 @@
+<?php
+class Cyclist extends User {
+    public function save()
+    {
+        $sql = "INSERT INTO cyclists (first_name, last_name, email, password, role_id) VALUES (:first_name, :last_name, :email, :password, :role_id)";
+        self::$db->query($sql);
+        self::$db->bind(':first_name', $this->first_name);
+        self::$db->bind(':last_name', $this->last_name);
+        self::$db->bind(':email', $this->email);
+        self::$db->bind(':password', $this->password);
+        self::$db->bind(':role_id', $this->role_id);
+
+        return self::$db->execute();
+    }
+}
