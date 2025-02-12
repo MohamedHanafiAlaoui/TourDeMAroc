7e0e580e9e660fdda4393925f799b144fdf0080b
fatima-ezzahraalouane <falouane38@gmail.com>
2025-02-12 14:54:45 +0100

    resolu un probleme de fichier config.php

--- app/Config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-    define('APPROOT', __DIR__ . "\../");
-    define('URLASSETS', 'http://localhost/tourDeMAroc/assets/');
-    define('URLROOT', 'http://localhost/tourDeMAroc/');
-    define('BASEPATH', '/tourDeMAroc');
-    define('DB_USER', "postgres");
-    define('DB_PASS', "mhmde0603");
-    define('DB_NAME', "tour-de-maroc");
-    define('DB_HOST', "localhost");
