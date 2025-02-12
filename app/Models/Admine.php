971c4c63412377ae9213e88f1ef70d241bd4e608
youssef elhoubi <elhoubiyoussef@gmail.com>
2025-02-12 09:58:27 +0100

    made the mailer helper and made some changes to the admine model

--- app/Models/Admine.php
+++ app/Models/Admine.php
@@ -17,6 +17,21 @@ class Admine extends User {
     }
     public function platformStatiscs(){
         $query = "SELECT count(*) as totalCyclest FROM cyclists WHERE approved = TRUE ";
+        self::$db->query($query);
+        $totaleApprovedCyclests = self::$db->single();
+        // totale number of users
+        $query = "SELECT count(*) as totalUsers FROM users ";
+        self::$db->query($query);
+        $totaleUsers = self::$db->single();
+        // team with the most number of approved cyclists
+        $query = "SELECT t.name FROM teams t JOIN cyclists c ON c.id_team = t.id_team AND c.approved = TRUE GROUP BY t.name ORDER BY count(*) DESC LIMIT 1 ";
+        self::$db->query($query);
+        $teamWithMostPlayers = self::$db->single();
+        // un resoved repportes 
+        $query = "SELECT count(*) as totalUnresolved FROM reports WHERE is_archived = FALSE ";
+        self::$db->query($query);
+        $unresolvedReports = self::$db->single();
+        return ["totalApprovedCyclests" => $totaleApprovedCyclests,"totalUsers" => $totaleUsers,"teamWithMostPlayers" => $teamWithMostPlayers,"unresolvedReports" => $unresolvedReports];
     }
 }
 ?>
